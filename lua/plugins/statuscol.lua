4e8be375d053caedfcf851105d661ce9c28257ef
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-02-27 00:48:30 +0200

    Added todo-comments.nvim to statuscol

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -6,15 +6,17 @@ return {
 			-- configuration goes here, for example:
 			relculright = false,
 			segments = {
-				{ sign = { name = { 'Dap' }, maxwidth = 1, auto = false },        click = 'v:lua.ScSa' },
+				{ sign = { name = { 'Bookmark' }, maxwidth = 1, auto = true }, click = 'v:lua.ScSa' },
+				{ sign = { name = { 'signs*' }, maxwidth = 1, auto = true },   click = 'v:lua.ScSa' },
+				{ sign = { name = { 'Dap' }, maxwidth = 1, auto = true },      click = 'v:lua.ScSa' },
 				{
 					sign = { namespace = { 'gitsigns*' }, maxwidth = 1, colwidth = 1, auto = false },
 					click = 'v:lua.ScSa',
 				},
-				{ sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = false }, click = 'v:lua.ScSa' },
+				{ sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = true }, click = 'v:lua.ScSa' },
 
-				{ text = { builtin.foldfunc, ' ' },                               click = 'v:lua.ScFa' },
-				{ text = { builtin.lnumfunc, '  ' },                              click = 'v:lua.ScLa' },
+				{ text = { builtin.foldfunc, ' ' },                              click = 'v:lua.ScFa' },
+				{ text = { builtin.lnumfunc, '  ' },                             click = 'v:lua.ScLa' },
 			}
 		})
 	end,
