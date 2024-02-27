4abc2031795e76887a9dca5f258c7f4269b7b85d
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-02-27 09:15:33 +0200

    Moved telscope fzf ext and added some comments

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -50,14 +50,12 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 
+-- ufo.nvim settings
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-
 vim.g.copilot_no_tab_map = true
-
-pcall(require('telescope').load_extension, 'fzf')
 
 require 'options.keymaps'
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -40,6 +40,8 @@ return {
           },
         },
       }
+
+      require('telescope').load_extension 'fzf'
       require('telescope').load_extension 'file_browser'
       require('telescope').load_extension 'ui-select'
       require("telescope").load_extension "undo"
