246c45b9020d09fe583f7ba2b0ee2adfcc49bc36
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-03-14 23:37:01 +0200

    refactor

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -106,7 +106,6 @@ return {
       ensure_installed = vim.tbl_keys(servers),
     }
 
-    local noop = function() end
     mason_lspconfig.setup_handlers {
       function(server_name)
         require('lspconfig')[server_name].setup {
