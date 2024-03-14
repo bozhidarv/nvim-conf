c5e81d0d4b290b57321a5373d27128acf197dba1
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-03-14 23:37:42 +0200

    fix: harpoon: fix setup

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,6 +3,11 @@ return {
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("harpoon").setup({})
+		require("harpoon"):setup({
+			settings = {
+				save_on_toggle = true,
+				sync_on_ui_close = true,
+			}
+		})
 	end
 }
