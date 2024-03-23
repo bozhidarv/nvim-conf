f912edddfd1f365673182d5be1bdba4473356df0
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-03-23 21:36:54 +0200

    Added hidden false

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.hidden = false
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
