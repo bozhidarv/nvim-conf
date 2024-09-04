d732404fb52ad3490913c27610c83d33e98fd952
Bozhidar Videnov <bozhidar.videnov@kongsbergdigital.com>
2024-09-04 15:01:06 +0300

    fix: got mbbill/undotree working on win32
    for more info on the solution and why it is necessary: https://github.com/mbbill/undotree/issues/168

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -35,6 +35,8 @@ vim.o.undofile = true
 --
 if vim.fn.has 'win32' == 1 then
   vim.opt.shell = 'pwsh'
+  --Fix for nvim not including diff and windows not having it in PATH
+  vim.g.undotree_DiffCommand = vim.fn.stdpath 'config' .. '\\bin\\diff.exe'
 end
 
 -- Case-insensitive searching UNLESS \C or capital in search
@@ -64,7 +66,7 @@ vim.bo.softtabstop = 2
 
 -- ufo.nvim settings
 vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
