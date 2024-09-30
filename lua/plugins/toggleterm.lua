3ef6c55b89d49e92af3873abefa8b230d41f3777
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-09-30 20:56:38 +0300

    fix: toggleterm open_mapping

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
     version = '*',
     config = function()
       require('toggleterm').setup {
-        open_mapping = { [[C-\>]], '<C-t>' },
+        open_mapping = { '<C-\\>', '<C-t>' },
         start_in_insert = true,
         direction = 'float',
       }
