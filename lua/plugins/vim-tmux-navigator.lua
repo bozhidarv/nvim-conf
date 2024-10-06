2792c2a277c20bba174adec77d1e7635fdde16c1
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-06 19:21:59 +0300

    refactor(tmux-navigator): migrated to smart splits

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    'christoomey/vim-tmux-navigator',
-    cond = vim.fn.has 'win32' == 0,
-  },
+  -- {
+  --   'christoomey/vim-tmux-navigator',
+  --   cond = vim.fn.has 'win32' == 0,
+  -- },
   {
     'mrjones2014/smart-splits.nvim',
-    cond = vim.fn.has 'win32' == 1,
+    -- cond = vim.fn.has 'win32' == 1,
   },
 }
