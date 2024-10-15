c690bcc5d928d8d432f73b4dc18ae90c3dbcb57c
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-15 22:45:12 +0300

    fix: oil-vcs-status

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,9 +2,10 @@ local add = MiniDeps.add
 
 add {
   source = 'stevearc/oil.nvim',
-  depends = {
-    -- 'SirZenith/oil-vcs-status',
-  },
+}
+
+add {
+  source = 'SirZenith/oil-vcs-status',
 }
 
 require('oil').setup {
@@ -188,34 +189,34 @@ require('oil').setup {
   },
 }
 
--- local status_const = require 'oil-vcs-status.constant.status'
---
--- local StatusType = status_const.StatusType
---
--- require('oil-vcs-status').setup {
---   status_symbol = {
---     [StatusType.Added] = '',
---     [StatusType.Copied] = '󰆏',
---     [StatusType.Deleted] = '',
---     [StatusType.Ignored] = '',
---     [StatusType.Modified] = '',
---     [StatusType.Renamed] = '',
---     [StatusType.TypeChanged] = '󰉺',
---     [StatusType.Unmodified] = ' ',
---     [StatusType.Unmerged] = '',
---     [StatusType.Untracked] = '',
---     [StatusType.External] = '',
---
---     [StatusType.UpstreamAdded] = '󰈞',
---     [StatusType.UpstreamCopied] = '󰈢',
---     [StatusType.UpstreamDeleted] = '',
---     [StatusType.UpstreamIgnored] = ' ',
---     [StatusType.UpstreamModified] = '󰏫',
---     [StatusType.UpstreamRenamed] = '',
---     [StatusType.UpstreamTypeChanged] = '󱧶',
---     [StatusType.UpstreamUnmodified] = ' ',
---     [StatusType.UpstreamUnmerged] = '',
---     [StatusType.UpstreamUntracked] = ' ',
---     [StatusType.UpstreamExternal] = '',
---   },
--- }
+local status_const = require 'oil-vcs-status.constant.status'
+
+local StatusType = status_const.StatusType
+
+require('oil-vcs-status').setup {
+  status_symbol = {
+    [StatusType.Added] = '',
+    [StatusType.Copied] = '󰆏',
+    [StatusType.Deleted] = '',
+    [StatusType.Ignored] = '',
+    [StatusType.Modified] = '',
+    [StatusType.Renamed] = '',
+    [StatusType.TypeChanged] = '󰉺',
+    [StatusType.Unmodified] = ' ',
+    [StatusType.Unmerged] = '',
+    [StatusType.Untracked] = '',
+    [StatusType.External] = '',
+
+    [StatusType.UpstreamAdded] = '󰈞',
+    [StatusType.UpstreamCopied] = '󰈢',
+    [StatusType.UpstreamDeleted] = '',
+    [StatusType.UpstreamIgnored] = ' ',
+    [StatusType.UpstreamModified] = '󰏫',
+    [StatusType.UpstreamRenamed] = '',
+    [StatusType.UpstreamTypeChanged] = '󱧶',
+    [StatusType.UpstreamUnmodified] = ' ',
+    [StatusType.UpstreamUnmerged] = '',
+    [StatusType.UpstreamUntracked] = ' ',
+    [StatusType.UpstreamExternal] = '',
+  },
+}
