e9e86a73069f9aeef59c82e4e9e0c713245be8e5
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-18 20:01:02 +0300

    fix(nvim-cmp): fix cmp.mapping.scroll_docs() mappings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -48,8 +48,8 @@ local cmp_options = {
       select = true,
     },
     ['<C-e>'] = cmp.mapping.close(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
