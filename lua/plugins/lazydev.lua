4b4984ee54370edd32a3aba69930daf5de445a0d
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-20 12:51:04 +0300

    fix(lazydev): lazydev not finding the installed libraries

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -4,13 +4,16 @@ add {
   source = 'folke/lazydev.nvim',
   depends = {
     'Bilal2453/luvit-meta',
-    'justinsgithub/wezterm-types'
-  }
+    'justinsgithub/wezterm-types',
+  },
 }
 
 require('lazydev').setup {
-  -- See the configuration section for more details
-  -- Load luvit types when the `vim.uv` word is found
-  { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-  { path = 'wezterm-types',      mods = { 'wezterm' } },
+  library = {
+    -- See the configuration section for more details
+    -- Load luvit types when the `vim.uv` word is found
+
+    { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+    { path = 'wezterm-types',      mods = { 'wezterm' } },
+  },
 }
