5fc7b5e290134eba2cb5bc7aac7aab908e9b3ff4
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-22 11:53:39 +0300

    feat: add textobject keymaps
    ib, ab - inside/around code block
    ]a, [a - goto next/previous start prarameter outer
    ]A, [A - goto next/previous end prarameter outer

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -16,6 +16,7 @@ add {
 add {
   source = 'nvim-treesitter/nvim-treesitter-textobjects',
 }
+
 -- Possible to immediately execute code which depends on the added plugin
 ---@diagnostic disable-next-line: missing-fields
 require('nvim-treesitter.configs').setup {
@@ -46,6 +47,8 @@ require('nvim-treesitter.configs').setup {
         ['if'] = '@function.inner',
         ['ac'] = '@class.outer',
         ['ic'] = '@class.inner',
+        ['ib'] = '@block.inner',
+        ['ab'] = '@block.outer',
       },
     },
     move = {
@@ -53,18 +56,22 @@ require('nvim-treesitter.configs').setup {
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
         [']m'] = '@function.outer',
+        [']a'] = '@parameter.outer',
         [']]'] = '@class.outer',
       },
       goto_next_end = {
         [']M'] = '@function.outer',
+        [']A'] = '@parameter.outer',
         [']['] = '@class.outer',
       },
       goto_previous_start = {
         ['[m'] = '@function.outer',
+        ['[a'] = '@parameter.outer',
         ['[['] = '@class.outer',
       },
       goto_previous_end = {
         ['[M'] = '@function.outer',
+        ['[A'] = '@parameter.outer',
         ['[]'] = '@class.outer',
       },
     },
@@ -78,4 +85,23 @@ require('nvim-treesitter.configs').setup {
       },
     },
   },
+}
+
+add {
+  source = 'nvim-treesitter/nvim-treesitter-context',
+}
+
+require('treesitter-context').setup {
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 20, -- The Z-index of the context window
+  on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 }
