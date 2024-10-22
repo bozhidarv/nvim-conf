3ea41ad56ab8a100207ae2cca9e46fbae799b2ad
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-22 12:10:29 +0300

    refactor: use mini.cursorword's default config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,4 @@
-require('mini.cursorword').setup {
-  -- Delay (in ms) between when cursor moved and when highlighting appeared
-  delay = 100,
-}
+require('mini.cursorword').setup()
 
 require('mini.indentscope').setup {
   draw = {
