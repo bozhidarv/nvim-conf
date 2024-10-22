5cda6a5f30129360c58d8d79f1ac86033da2faa4
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-10-22 11:47:53 +0300

    fix: load 'folke/todo-comments.nvim' on start

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,10 +1,11 @@
 local add = MiniDeps.add
 
 add {
-  source='folke/trouble.nvim',
+  source = 'folke/trouble.nvim',
   depends = {
-    'folke/todo-comments.nvim'
-  }
+    'folke/todo-comments.nvim',
+  },
 }
 
 require('trouble').setup()
+require('todo-comments').setup()
