6fe347f4b63403511b165c21efa2556dd3867530
Bozhidar Videnov <bozh.bv03@gmail.com>
2025-02-22 13:15:19 +0200

    feat(autcmd): add autocmd to set compiler and add the qf errors to diagnostics

--- lua/autogroups/init.lua
+++ lua/autogroups/init.lua
@@ -133,6 +133,34 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'InsertLeave' }, {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  group = vim.api.nvim_create_augroup('set-compiler', { clear = true }),
+  ---@param event event_args
+  callback = function(event)
+    local compilers_by_ft = {
+      zig = 'zig_build',
+    }
+
+    if compilers_by_ft[vim.bo.filetype] == nil then
+      return
+    end
+    vim.cmd('compiler ' .. compilers_by_ft[vim.bo.filetype])
+    vim.lsp.codelens.refresh { bufnr = event.buf }
+
+    vim.keymap.set('n', '<leader>cm', ':make<CR>', { buffer = event.buf, desc = 'Compile' })
+  end,
+})
+
+-- Autocommand to run after :make
+vim.api.nvim_create_autocmd('QuickFixCmdPost', {
+  pattern = 'make',
+  callback = function()
+    local qflist = vim.fn.getqflist()
+    local diagnostics = vim.diagnostic.fromqflist(qflist)
+    vim.diagnostic.set(vim.api.nvim_create_namespace 'make_diagnostics', 0, diagnostics)
+  end,
+})
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
   ---@param event event_args
